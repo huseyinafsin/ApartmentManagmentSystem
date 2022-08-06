53f717b78b41c55e20387a88fda4231904f866c0
huseyinafsin <huseyinafsin4@gmail.com>
2022-08-06 22:28:41 +0300

    CCV

---
+++ Bussiness/Configuration/Log/LogFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bussiness.Configuration.Log
+{
+    public class LogFilterAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var msLogger = context.HttpContext.RequestServices.GetService<MongoDbLogger>();
+            var data = context.ActionArguments.Values;
+            var logStr = System.Text.Json.JsonSerializer.Serialize(data);
+
+            msLogger.LoggerManager.Information(logStr);
+
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine("İşlem tamamlandı");
+        }
+    }
+}
---
+++ Bussiness/Configuration/Log/MongoDbLogger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using PaymentService.Models;
+using Serilog;
+
+namespace Bussiness.Configuration.Log
+{
+    public class MongoDbLogger
+    {
+        public ILogger LoggerManager;
+        private readonly MongoDbSettings settings;
+
+
+        public MongoDbLogger(IConfiguration configuration, IOptions<MongoDbSettings> options)
+        {
+            this.settings = options.Value;
+            var mongoClient = new MongoClient(settings.ConnectionString).GetDatabase(settings.Database);
+
+            LoggerManager = new LoggerConfiguration().WriteTo.MongoDB(mongoClient,collectionName:"log").CreateLogger();
+
+
+        }
+    }
+}
